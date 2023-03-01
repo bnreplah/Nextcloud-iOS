6d6a8b2fcc800b909f1f23c1b6b96e9afb86fcc3
Ben <46547251+bnreplah@users.noreply.github.com>
2023-03-01 08:53:53 -0800

    Delete appcenter-pre-build.sh
    Signed-off-by: Ben <46547251+bnreplah@users.noreply.github.com>

--- appcenter-pre-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Example: Change bundle name of an iOS app for non-production
-if [ "$APPCENTER_BRANCH" != "main" ];
-then
-    plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
-fi
