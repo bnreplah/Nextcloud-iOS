7b5882e750bb47d07a91416e5bb172baac347abd
Ben <46547251+bnreplah@users.noreply.github.com>
2023-09-13 13:03:18 -0700

    Update appcenter-post-build.sh
    added update to Gen-Ir functionality 
    
    Signed-off-by: Ben <46547251+bnreplah@users.noreply.github.com>

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,6 +1,7 @@
 brew tap veracode/tap
 brew install gen-ir
 carthage update --use-xcframeworks --platform iOS
-xcodebuild archive -project Nextcloud.xcodeproj  -configuration Debug -scheme Nextcloud -destination generic/platform=iOS DEBUG_INFORMATION_FORMAT=dwarf-with-dsym -archivePath Nextcloud.xcarchive CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO ENABLE_BITCODE=NO | tee build_log.txt
-gen-ir build_log.txt Nextcloud.xcarchive/IR
+xcodebuild archive -project Nextcloud.xcodeproj -configuration Debug -scheme Nextcloud -destination generic/platform=iOS DEBUG_INFORMATION_FORMAT=dwarf-with-dsym -archivePath Nextcloud.xcarchive CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO ENABLE_BITCODE=NO | tee build_log.txt
+gen-ir build_log.txt Nextcloud.xcarchive --project-path Nextcloud.xcodeproj
+ls Nextcloud.xcarchive/*
 zip -r Nextcloud.zip Nextcloud.xcarchive
